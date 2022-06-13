86535c7391a8f58fd383ae6a50156e6cbaebef74
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-13 12:23:36 -0400

    binary trees

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,51 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+
+    return nil if @root.nil?
+
+    cur = @root
+    found = false
+
+    while cur && !found
+
+      if cur.value > value
+        cur = cur.left
+      elsif cur.value < value
+        cur = cur.right
+      else 
+        found = true
+      end
+    end
+    found ? cur : nil
   end
 
+
   def insert(value)
-    # your code here
+    new_node = Node.new(value)
+
+    if @root.nil?
+      @root = new_node
+      return @root
+    end
+
+    cur = @root
+    while cur
+      return nil if value == cur.value
+      if value < cur.value
+        if cur.left.nil?
+          cur.left = new_node
+          return cur.left
+        end
+      cur = cur.left
+      else 
+        if cur.right.nil?
+        cur.right = new_node
+        return cur.right
+        end
+        cur = cur.right
+      end
+    end
   end
 
 end 
